48111ea62a6e0744fe145d384afdc4aa4b79717f
Christine Manna <cimanna@gmail.com>
2022-06-28 12:18:47 -0400

    basic page layout

--- Recipe-App/recipe.js
+++ Recipe-App/recipe.js
@@ -1,5 +1,6 @@
+const resultDiv = document.querySelector(".result-container")
+// let healthCont = document.getElementById(`${res.hits[i].recipe.url}`);
 document.querySelector("form").addEventListener("submit", function (event) {
-  // console.log(search)
   event.preventDefault();
   let option = document.querySelector(".inputField").value;
 
@@ -10,13 +11,21 @@ document.querySelector("form").addEventListener("submit", function (event) {
     .then((res) => {
       console.log(res);
       let unList = document.createElement("ul");
+
       for (let i = 0; i < res.hits.length; i++) {
         console.log(res.hits[i].recipe.label);
         let li = document.createElement("li");
-        li.innerText = res.hits[i].recipe.label;
+        let textEl = document.createElement('p')
+        textEl.innerText = res.hits[i].recipe.label;
+        textEl.classList.add('name')
+        li.appendChild(textEl)
+        let container = document.createElement('div')
+        container.classList.add('contain')
         let thum = document.createElement("img");
+        thum.classList = "thum-img";
         thum.src = res.hits[i].recipe.images.SMALL.url;
-        li.appendChild(thum);
+        container.appendChild(thum);
+        li.appendChild(container)
         unList.appendChild(li);
 
         li.addEventListener("click", function () {
@@ -26,98 +35,50 @@ document.querySelector("form").addEventListener("submit", function (event) {
           let modalInfo = `
           <div class="modal-content">
             <span class="close">&times;</span>
-            <img src='${res.hits[i].recipe.image}'/>
-            <ul class="health-labels" id="${res.hits[i].recipe.url}">
+            <img src="${res.hits[i].recipe.image}"/>
+            <h1>${res.hits[i].recipe.label}</h1>
+            <ul id="${res.hits[i].recipe.url}">
+            <h2>Health Concerns / Allergies</h2>
             </ul>
-            <a target="_blank" rel="noopener noreferrer" href="${res.hits[i].recipe.url}">Recipe Page</a>
+            <ul class='nutrition'>
+            <h2>Basic Nutrition Facts</h2> 
+              <li>Calories: ${Math.round(res.hits[i].recipe.totalNutrients.ENERC_KCAL.quantity)} ${res.hits[i].recipe.totalNutrients.ENERC_KCAL.unit}</li>
+              <li>Servings: ${res.hits[i].recipe.yield}</li>
+              <li>Fat: ${Math.round(res.hits[i].recipe.totalNutrients.FAT.quantity)} ${res.hits[i].recipe.totalNutrients.FAT.unit}</li>
+              <li>Saturated Fat: ${Math.round(res.hits[i].recipe.totalNutrients.FASAT.quantity)} ${res.hits[i].recipe.totalNutrients.FASAT.unit}</li>
+              <li>Carbohydrates: ${Math.round(res.hits[i].recipe.totalNutrients.CHOCDF.quantity)} ${res.hits[i].recipe.totalNutrients.CHOCDF.unit}</li>
+              <li>Protein: ${Math.round(res.hits[i].recipe.totalNutrients.PROCNT.quantity)} ${res.hits[i].recipe.totalNutrients.PROCNT.unit}</li>
+              <li>Sugar: ${Math.round(res.hits[i].recipe.totalNutrients.SUGAR.quantity)} ${res.hits[i].recipe.totalNutrients.SUGAR.unit}</li>
+              <li>Fiber: ${Math.round(res.hits[i].recipe.totalNutrients.FIBTG.quantity)} ${res.hits[i].recipe.totalNutrients.FIBTG.unit}</li>
+            </ul>
+              <a href="${res.hits[i].recipe.url}" rel="noopener noreferrer" target="_blank">Recipe Page</a>
           </div>
           `;
 
           modal.insertAdjacentHTML("beforeend", modalInfo);
           let span = document.getElementsByClassName("close")[0];
-          // When the user clicks on <span> (x), close the modal
+          //close the modal when the user clicks on 'x' 
           span.onclick = function () {
             modal.style.display = "none";
           };
-
-          // When the user clicks anywhere outside of the modal, close it
+          // close it when the user clicks anywhere outside of the modal 
           window.onclick = function (event) {
             if (event.target == modal) {
               modal.style.display = "none";
             }
           };
-          // let cont = document.createElement('div')
-          // let image = document.createElement('img')
-          // image.src = res.hits[i].recipe.image
-          // cont.appendChild(image)
           let healthCont = document.getElementById(`${res.hits[i].recipe.url}`);
-          let healthLabel = "";
+          let healthLabel = ""
 
           for (let j = 0; j < res.hits[i].recipe.healthLabels.length; j++) {
             healthLabel += `<li>${res.hits[i].recipe.healthLabels[j]}</li>`;
-
-            // let healthLI = document.createElement('li')
-            // healthLI.innerText = res.hits[i].recipe.healthLabels[j]
-            // healthLab.appendChild(healthLI)
-            // let nutFacts = document.createElement('ul')
-            // let nut = res.hits[i].recipe.totalDaily
+  
           }
-          healthCont.insertAdjacentHTML("beforeend", healthLabel);
+          healthCont.insertAdjacentHTML("beforeend", healthLabel);          
         });
       }
-      document.body.appendChild(unList);
+      resultDiv.innerHTML=""
+      resultDiv.appendChild(unList);
     });
-});
+  })
 
-// let recipeArr = []
-// let searchButton = document.querySelector('.searchButton')
-// let recipeNames = document.querySelector('.recipe-names')
-// let recipeImage = document.querySelector('.recipeImage')
-
-// recipes.then(res => res.json())
-//   data.preventDefault()
-
-//   .then((data) => {
-//   recipeArr.push(data.hits)
-
-//   recipeName.innerHTML = `${recipeArr[4][0][0].label}`
-//   recipeImage.src = recipes[4][0].recipe.image)
-// }
-// let openEls = document.querySelectorAll("[data-open]");
-// let isVisible = "is-visible";
-// let closeEls = document.querySelectorAll("[data-close]");
-
-// for(let el of openEls) {
-//   el.addEventListener("click", function() {
-//     const modalId = this.dataset.open;
-//     document.getElementById(modalId).classList.add(isVisible);
-//   });
-// }
-
-// for (let el of closeEls) {
-//   el.addEventListener("click", function() {
-//     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
-//   });
-// }
-
-// document.addEventListener("keyup", e => {
-//   if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
-//     document.querySelector(".modal.is-visible").classList.remove(isVisible);
-//   }
-// });
-// let searchRecipe = (event) => {
-//   event.preventDefault()
-
-//   let result = document.querySelector('.input').value
-//   let newUrl = `https://api.thecatapi.com/v1/images/search?category_ids=${result}`
-
-//   fetch(newUrl, {
-//     headers: {
-//       'x-api-key': '94031ae7cc4322e3ef2c5e273907f2fc'
-//     }
-//   })
-//   .then(res => res.json())
-//   .then(res => document.querySelector('.categoryCatImage').src = res[0].url)
-// }
-// randomButton.addEventListener('click', randomCat)
-// searchButton.addEventListener('click', searchCat)
